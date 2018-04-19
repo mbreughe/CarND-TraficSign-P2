aca490c408af9f2c984a04d16f63b7ece012f5ba
Maximilien Breughe <mbreughe@nvidia.com>
2018-04-19 18:09:58 -0500

    Validation accuracy of 0.943
    Removed 1st maxpool, increase FC sizes, added dropout

--- lenet.py
+++ lenet.py
@@ -15,10 +15,7 @@ def LeNet(x, classes):
     # SOLUTION: Activation.
     conv1 = tf.nn.relu(conv1)
 
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
+    # SOLUTION: Layer 2: Convolutional. Output = 24x24x16.
     conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
     conv2_b = tf.Variable(tf.zeros(16))
     conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
@@ -26,30 +23,31 @@ def LeNet(x, classes):
     # SOLUTION: Activation.
     conv2 = tf.nn.relu(conv2)
 
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+    # SOLUTION: Pooling. Input = 24x24x16. Output = 12x12x16.
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+    # SOLUTION: Flatten. Input = 5x5x16. Output = 2304.
     fc0   = flatten(conv2)
 
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
+    # SOLUTION: Layer 3: Fully Connected. Input = 2304. Output = 400.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(2304, 400), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(400))
     fc1   = tf.matmul(fc0, fc1_W) + fc1_b
 
     # SOLUTION: Activation.
     fc1    = tf.nn.relu(fc1)
+    drop = tf.nn.dropout(fc1, 0.5)
 
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    # SOLUTION: Layer 4: Fully Connected. Input = 400. Output = 120.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(120))
+    fc2    = tf.matmul(drop, fc2_W) + fc2_b
 
     # SOLUTION: Activation.
     fc2    = tf.nn.relu(fc2)
 
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = classes.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, classes), mean = mu, stddev = sigma))
+    # SOLUTION: Layer 5: Fully Connected. Input = 120. Output = classes.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(120, classes), mean = mu, stddev = sigma))
     fc3_b  = tf.Variable(tf.zeros(classes))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
 
